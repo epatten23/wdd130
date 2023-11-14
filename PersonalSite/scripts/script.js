09c67db4ec5304b85add45182a7ed96b59512587
epatten23 <epatten@byui.edu>
2023-11-14 10:51:45 -0700

    forum

--- PersonalSite/scripts/script.js
+++ PersonalSite/scripts/script.js
@@ -1,11 +1,231 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    var y = document.getElementById("Braun");
-    if (y.style.display === "none") {
-        y.style.display = "block";
-        x.style.display = "none";
-      } else {
-        y.style.display = "none";
-        x.style.display = "block";
-      }
-  }
+function Braun() {
+  var x = document.getElementById("Braunoff");
+  var y = document.getElementById("Braunon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function mpj() {
+  var x = document.getElementById("mpjoff");
+  var y = document.getElementById("mpjon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function strawther() {
+  var x = document.getElementById("strawtheroff");
+  var y = document.getElementById("strawtheron");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function tyson() {
+  var x = document.getElementById("tysonoff");
+  var y = document.getElementById("tysonon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+
+function caldwell() {
+  var x = document.getElementById("caldwelloff");
+  var y = document.getElementById("caldwellon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function dejordan() {
+  var x = document.getElementById("dejordanoff");
+  var y = document.getElementById("dejordanon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function jackson() {
+  var x = document.getElementById("jacksonoff");
+  var y = document.getElementById("jacksonon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function watson() {
+  var x = document.getElementById("watsonoff");
+  var y = document.getElementById("watsonon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function holiday() {
+  var x = document.getElementById("holidayoff");
+  var y = document.getElementById("holidayon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function holiday() {
+  var x = document.getElementById("holidayoff");
+  var y = document.getElementById("holidayon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function key() {
+  var x = document.getElementById("keyoff");
+  var y = document.getElementById("keyon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function jokic() {
+  var x = document.getElementById("jokicoff");
+  var y = document.getElementById("jokicon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function gillespie() {
+  var x = document.getElementById("gillespieoff");
+  var y = document.getElementById("gillespieon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function nnaji() {
+  var x = document.getElementById("nnajioff");
+  var y = document.getElementById("nnajion");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function pickett() {
+  var x = document.getElementById("pickettoff");
+  var y = document.getElementById("picketton");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function murray() {
+  var x = document.getElementById("murrayoff");
+  var y = document.getElementById("murrayon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function huff() {
+  var x = document.getElementById("huffoff");
+  var y = document.getElementById("huffon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function cancar() {
+  var x = document.getElementById("cancaroff");
+  var y = document.getElementById("cancaron");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+function gordon() {
+  var x = document.getElementById("gordonoff");
+  var y = document.getElementById("gordonon");
+  if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+    } else {
+      y.style.display = "none";
+      x.style.display = "block";
+    }
+}
+
+
+
